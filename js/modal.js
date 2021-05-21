ff1a9c085326d10ced6c30159a2fea4fb9b2d7a4
N I K I T A <62851883+coldxdev@users.noreply.github.com>
2021-05-21 21:07:39 +0300

    add burgermenu, modal scripts

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+const moreElems = document.querySelectorAll('.more');
+const modalElem = document.querySelector('.modal');
+
+const openModal = () => {
+    modalElem.classList.remove('hidden');
+    disableScroll();
+};
+
+const closeModal = () => {
+    modalElem.classList.add('hidden');
+    enableScroll();
+};
+
+moreElems.forEach(moreElem => {
+    moreElem.addEventListener('click', openModal);
+})
+
+
+modalElem.addEventListener('click', (event) => {
+    const target = event.target;
+    if(target.classList.contains('overlay') || 
+       target.classList.contains('modal__close')){
+        closeModal();
+    }
+})
